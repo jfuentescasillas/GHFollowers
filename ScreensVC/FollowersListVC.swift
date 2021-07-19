ad697efe3ed842490468e55b00b836234302152a
JorgeFuentesCasillas <j.fuentes.casillas@gmail.com>
2021-07-19 17:20:10 +0200

    [WIP]-FollowerCell and AvatarImageView added
    -Added FollowerCell which configures what the collectionView's cell will show (avatar image and usernameLabel)
    -Added GFAvatarImageView which contains the user's avatar image
    -FollowerListVC refactored in methods and createThreeCoplumnFlowLayoutMethod was added

--- ScreensVC/FollowersListVC.swift
+++ ScreensVC/FollowersListVC.swift
@@ -11,24 +11,16 @@ import UIKit
 class FollowersListVC: UIViewController {
 	// MARK: - Properties
 	var username: String!
+	var collectionView: UICollectionView!
 	
 
 	// MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
-		view.backgroundColor = .systemBackground
-		navigationController?.navigationBar.prefersLargeTitles = true
 		
-		NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-			switch result {
-			case .success(let followers):
-				print("Followers.count = \(followers.count)")
-				print("Followers = \(followers)")
-			case .failure(let error):
-				self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
-			}			
-		}
+		configureViewController()
+		configureCollectionView()
+		getFollowers()
     }
 	
 	
@@ -38,5 +30,51 @@ class FollowersListVC: UIViewController {
 		 
 		// Moving this line of code to viewWill Appear deals with a bug in which the NavigationBar was involved (it showed when it was not supposed to appear)
 		navigationController?.setNavigationBarHidden(false, animated: true)
+	}
+	
+	
+	// MARK: - configure Methods
+	private func configureViewController() {
+		view.backgroundColor = .systemBackground
+		navigationController?.navigationBar.prefersLargeTitles = true
+	}
+	
+	
+	func configureCollectionView() {
+		collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+		view.addSubview(collectionView)
+		
+		collectionView.backgroundColor = .systemPink
+		collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+	}
+	
+	
+	// MARK: - Get Followers
+	private func getFollowers() {
+		NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+			switch result {
+			case .success(let followers):
+				print("Followers.count = \(followers.count)")
+				print("Followers = \(followers)")
+			case .failure(let error):
+				self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
+			}
+		}
+	}
+	
+	
+	// MARK: - CreateFollowLayout Method
+	func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+		let width = view.bounds.width
+		let padding: CGFloat = 12
+		let minimumItemSpacing: CGFloat = 10
+		let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)  // We do minimum spacing * 2 because in a 3 column layout, there are 2 spaces between the 3 items. The space between the left item and the middle item. And the space between the middle item and the right item.
+		let itemWidth = availableWidth / 3
+		
+		let flowLayout = UICollectionViewFlowLayout()
+		flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+		flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+		
+		return flowLayout
 	}
 }
